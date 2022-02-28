13b1e83dffd0d6369e2b863710322757943bdba8
LemecRans <71868802+LemecRans@users.noreply.github.com>
2022-02-28 10:08:05 +0300

    Update Connexion.java

--- src/main/java/classmapping/Connexion.java
+++ src/main/java/classmapping/Connexion.java
@@ -6,18 +6,18 @@ import java.sql.DriverManager;
 public class Connexion {
 	public static Connection con() {
         java.sql.Connection conn = null;
-        /*try{
+        try{
             Class.forName("org.postgresql.Driver");
             conn = DriverManager.getConnection("jdbc:postgresql://ec2-3-216-113-109.compute-1.amazonaws.com/d17ine1i00j9d8","jirwafpelqqjpi","788d1e3bd8f63ddcc2271617e38c24639b7ddac4eb5739667f7ced5ffa220239");          
         } catch(Exception e){
             e.printStackTrace();
-        }*/
-        try{
+        }
+        /*try{
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ProjetMobile","root","root");          
         } catch(Exception e){
             e.printStackTrace();
-        }
+        }*/
         return conn;
     }
 }
